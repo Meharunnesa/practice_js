57211130acb6db2273b75be242e66751ac4d66b5
Meharunnesa <meharunnesab28@gmail.com>
2025-03-27 12:36:35 +0600

    Updated the file with new code

--- index.php
+++ index.php
@@ -9,12 +9,19 @@
  * Text Domain:       my-basics-plugin
 */
 
+function wm_styles(){
 
+    wp_enqueue_script( 'custom-script', plugin_dir_url(__FILE__) . 'js/script.js', array('jquery'), '1.0.0', true );
+    
+ }
+ 
+ 
+ add_action('admin_enqueue_scripts', 'wm_styles');
 
  ?>
 
 
-<script>
+
 
 <P id="demo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</P>
 
@@ -40,4 +47,5 @@
 
 <button onclick="document.getElementById('light').src='pic_bulboff.gif'">Turn off light</button>
 
-</script>
+
+
